657de5d7d4aa28a401ae35377a56e971276b55f6
paullasarev <paullasarev@gmail.com>
2014-11-01 19:29:03 +0400

    initial

---
+++ test-e2e/main-page.js
@@ -0,0 +1,8 @@
+// spec.js
+describe('angularjs homepage', function() {
+  it('should have a title', function() {
+    browser.get('http://localhost:3000/');
+
+    expect(browser.getTitle()).toEqual('Demo');
+  });
+});
---
+++ test/module.js
@@ -0,0 +1,5 @@
+describe('angular module', function(){
+  it('should have module', function(done){
+    done();
+  })
+})
